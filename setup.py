15e3d520f2c7e24159c426e2640aabaead7033e7
danmontesi <danmontesi@gmail.com>
2020-04-26 23:21:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ INSTALL_REQUIRES = (
     'scipy>=0.19.0',
     'networkx>=2.2',
     'matplotlib>=2.0.0',
-    'scikit-learn>=0.21.2',
+    'sklearn>=0.21.2',
     'theano>=0.9.0',
     'keras==2.0.2'
 )
